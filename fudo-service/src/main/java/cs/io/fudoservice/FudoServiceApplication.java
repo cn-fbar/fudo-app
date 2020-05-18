e7eca3ee8913d688f6a5182d7901b7c394157549
Deiveehan Nallazhagappan (LCL-C) <Deiveehan.Nallazhagappan@loblaw.ca>
2020-05-18 01:07:32 -0400

    Initial files

---
+++ fudo-service/src/main/java/cs/io/fudoservice/FudoServiceApplication.java
@@ -0,0 +1,13 @@
+package cs.io.fudoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FudoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FudoServiceApplication.class, args);
+	}
+
+}
---
+++ fudo-service/src/test/java/cs/io/fudoservice/FudoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cs.io.fudoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FudoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
